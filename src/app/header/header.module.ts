af1d88eb5f37b2059a3cb3f616d9b8ac7a6eab3e
tereza-tech <tereza.tech@gmail.com>
2022-02-12 20:09:40 +0000

    bootstrap added

---
+++ src/app/header/header.module.ts
@@ -0,0 +1,12 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from './header.component';
+
+@NgModule({
+  declarations: [ HeaderComponent ],
+  exports: [ HeaderComponent ],
+  imports: [ CommonModule ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+})
+export class HeaderModule {}
